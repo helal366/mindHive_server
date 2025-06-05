981581bfee1e8761b087fbf1774343d0215aeb4d
Helal366 <helal56876@gmail.com>
2025-06-06 01:16:31 +0600

    mongodb connection ok

--- index.js
+++ index.js
@@ -3,10 +3,44 @@ require('dotenv').config();
 const cors = require('cors');
 const app=express();
 const port=process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 // middlewire
 app.use(cors());
 app.use(express.json());
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/', (req,res)=>{
     res.send('Welcome to mind hive!')
@@ -15,8 +49,5 @@ app.listen(port, ()=>{
     console.log(`The server is running on the port, ${port}`)
 })
 
-// helalskr77
-// ka3OrqSipQCtXADp
 
-// mind-hive
-// ka3OrqSipQCtXADp
+
