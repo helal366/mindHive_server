51f9cfa5c42e8afbe564d323b75748652a030764
Helal366 <helal56876@gmail.com>
2025-06-06 00:17:51 +0600

    basic port setup ok

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const app=express();
+const port=process.env.PORT || 3000;
+
+// middlewire
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send('Welcome to mind hive!')
+});
+app.listen(port, ()=>{
+    console.log(`The server is running on the port, ${port}`)
+})
