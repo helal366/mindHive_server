3c9380b89284f9989e2ac584a37d7a9bab6b2ad3
Helal366 <helal56876@gmail.com>
2025-06-06 22:53:54 +0600

    get api created for all articles

--- index.js
+++ index.js
@@ -27,13 +27,18 @@ async function run() {
    
       const database=client.db('mindHive');
       const articlesCollection=database.collection('articles');
-
+    // post single article data
       app.post('/post-article', async(req,res)=>{
         const articleData=req.body;
         const result = await articlesCollection.insertOne(articleData);
         res.send(result);
       })
 
+      // get all article data
+      app.get('/articles',async(req,res)=>{
+        const allArticles=await articlesCollection.find().toArray();
+        res.send(allArticles)
+      })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
